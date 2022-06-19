77464ebd956d3c1f0a5a30ab71f94d83a48078ee
Zachary D. Skelton <zbert@hotmail.com>
2022-06-18 21:51:00 -0500

    Working with Button Options.

--- Tachometer.ino
+++ Tachometer.ino
@@ -10,8 +10,10 @@
  * 6/17/2022
  */
 
-// Pins
+// Pins (A4/A5 for SDA/SCL)
 #define IR 2
+#define BUTTON_RED 8
+#define BUTTON_GRN 7
 #define LED 13
 
 // Display
@@ -23,6 +25,19 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Globals
 int objects = 1;
+int state = 0;
+bool started = false;
+
+// Button States
+#define NONE 0
+#define B_RED 1
+#define B_GRN 2
+#define BOTH 3
+
+// Application States
+#define STATE_LOAD 0
+#define STATE_MENU 1
+#define STATE_MEASURE 2
 
 // Display Text
 void printText(String msg) {
@@ -35,30 +50,8 @@ void printText(String msg) {
   display.display();
 }
 
-// Initialize Device
-void setup() {  
-  // Start Serial
-  Serial.begin(9600);
-
-  // Start Screen
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed."));
-    for(;;); // Loop Forever
-  }
-
-  // Set Pin Mode
-  pinMode(IR, INPUT);
-  pinMode(LED, OUTPUT);
-
-  // Set up Devices
-  display.display();
-
-  // Clear the Buffer
-  display.clearDisplay();
-}
-
-// Main Loop
-void loop() {
+// Capture and Calculate RPM
+void measureRPM() {
   // Local Variables
   float _timer = 0;
   float _rpm = 0;
@@ -92,7 +85,94 @@ void loop() {
   Serial.print("RPM: ");
   Serial.print(_rpm);
   Serial.print("\n");
+}
 
+int getButtons() {
+  // Variables
+  int _key = NONE;
+  bool _red = digitalRead(BUTTON_RED) == HIGH ? true : false;
+  bool _grn = digitalRead(BUTTON_GRN) == HIGH ? true : false;
+
+  // Conditions
+  if (!_red && !_grn) {
+    _key = NONE;
+  } else if (_red && !_grn) {
+    _key = B_RED;
+  } else if (!_red && _grn) {
+    _key = B_GRN;
+  } else if (_red && _grn) {
+    _key = BOTH;
+  }
+  
+  // Return
+  return _key;
+}
+
+// Initialize Device
+void setup() {  
+  // Start Serial
+  Serial.begin(9600);
+
+  // Start Screen
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed."));
+    for(;;); // Loop Forever
+  }
+
+  // Set Pin Mode
+  pinMode(IR, INPUT);
+  pinMode(BUTTON_RED, INPUT);
+  pinMode(BUTTON_GRN, INPUT);
+  pinMode(LED, OUTPUT);
+
+  // Set up Devices
+  display.display();
+
+  // Set State
+  state = STATE_MENU;
+
+  // Clear the Buffer
+  display.clearDisplay();
+}
+
+// Main Loop
+void loop() {
+  // Handle Button Pushes
+  int _butts = getButtons();
+  switch (_butts) {
+    case NONE:
+      break;
+    case B_GRN:
+      if (state == STATE_MENU) {
+        objects++;
+      }
+      break;
+    case B_RED:
+      if (state == STATE_MENU) {
+        if (objects > 1) {
+          objects--;
+        }
+      }
+      break;
+    case BOTH:
+      if (state == STATE_MEASURE) {
+        state = STATE_MENU;
+        digitalWrite(LED, LOW);
+      } else {
+        state = STATE_MEASURE;
+        digitalWrite(LED, HIGH);
+      }
+      break;
+  }
+
+  // Display Based on State
+  if (state == STATE_MEASURE) {
+    measureRPM();
+  } else {
+    String message = String("Objects: " + String(objects));
+    printText(message);
+  }
+  
   // Wait
   delay(100);
 }
